55454801216cfd4c87a54081d24f1457703a10e3
David Evans <davidje13@users.noreply.github.com>
2018-09-08 20:39:38 +0100

    Extract functions in entryUtils

--- fetch/entryUtils.js
+++ fetch/entryUtils.js
@@ -100,125 +100,127 @@ define([
 		return 'return {' + parts + '};';
 	}
 
-	return {
-		compile: (code, parameters, {pre = '', returning = null} = {}) => {
-			let after = '';
-			if(returning !== null) {
-				after = buildFunctionFinder(code, returning);
-			}
+	function compile(code, parameters, {pre = '', returning = null} = {}) {
+		let after = '';
+		if(returning !== null) {
+			after = buildFunctionFinder(code, returning);
+		}
 
-			// Wrap code in function which blocks access to obviously dangerous
-			// globals (this wrapping cannot be relied on as there may be other
-			// ways to access global scope, but should prevent accidents - other
-			// measures must be used to prevent malice)
-			const src = (
-				// All prelude is rendered on 1 line so that line numbers in
-				// reported errors are easy to rectify
-				'"use strict";' +
-				'self.tempFn = function(parameters, extras) {' +
-					'const self = undefined;' +
-					'const window = undefined;' +
-					'const require = undefined;' +
-					'const requireFactory = undefined;' +
-					'const define = undefined;' +
-					'const addEventListener = undefined;' +
-					'const removeEventListener = undefined;' +
-					'const postMessage = undefined;' +
-					'const Date = undefined;' +
-					'const performance = undefined;' +
-					'const console = (extras.consoleTarget ?' +
-					'((({consoleTarget, consoleLimit = 100, consoleItemLimit = 1024}) => {' +
-						'const dolog = (type, values) => {' +
-							'consoleTarget.push({' +
-								'type,' +
-								'value: Array.prototype.map.call(values, (v) => {' +
-									'if(v && v.message) {' +
-										'return String(v.message);' +
-									'}' +
-									'try {' +
-										'return JSON.stringify(v);' +
-									'} catch(e) {' +
-										'return String(v);' +
-									'}' +
-								'}).join(" ").substr(0, consoleItemLimit),' +
-							'});' +
-							'if(consoleTarget.length > consoleLimit) {' +
-								'consoleTarget.shift();' +
-							'}' +
-						'};' +
-						'return {' +
-							'clear: () => {consoleTarget.length = 0;},' +
-							'info: function() {dolog("info", arguments);},' +
-							'log: function() {dolog("log", arguments);},' +
-							'warn: function() {dolog("warn", arguments);},' +
-							'error: function() {dolog("error", arguments);},' +
-						'};' +
-					'})(extras)) : undefined);' +
-					pre +
-					'extras = undefined;' +
-					'return (function({' + parameters.join(',') + '}) {\n' +
-						code + '\n' + after +
-					'}).call(parameters["this"] || {}, parameters);' +
-				'};\n'
-			);
+		// Wrap code in function which blocks access to obviously dangerous
+		// globals (this wrapping cannot be relied on as there may be other
+		// ways to access global scope, but should prevent accidents - other
+		// measures must be used to prevent malice)
+		const src = (
+			// All prelude is rendered on 1 line so that line numbers in
+			// reported errors are easy to rectify
+			'"use strict";' +
+			'self.tempFn = function(parameters, extras) {' +
+				'const self = undefined;' +
+				'const window = undefined;' +
+				'const require = undefined;' +
+				'const requireFactory = undefined;' +
+				'const define = undefined;' +
+				'const addEventListener = undefined;' +
+				'const removeEventListener = undefined;' +
+				'const postMessage = undefined;' +
+				'const Date = undefined;' +
+				'const performance = undefined;' +
+				'const console = (extras.consoleTarget ?' +
+				'((({consoleTarget, consoleLimit = 100, consoleItemLimit = 1024}) => {' +
+					'const dolog = (type, values) => {' +
+						'consoleTarget.push({' +
+							'type,' +
+							'value: Array.prototype.map.call(values, (v) => {' +
+								'if(v && v.message) {' +
+									'return String(v.message);' +
+								'}' +
+								'try {' +
+									'return JSON.stringify(v);' +
+								'} catch(e) {' +
+									'return String(v);' +
+								'}' +
+							'}).join(" ").substr(0, consoleItemLimit),' +
+						'});' +
+						'if(consoleTarget.length > consoleLimit) {' +
+							'consoleTarget.shift();' +
+						'}' +
+					'};' +
+					'return {' +
+						'clear: () => {consoleTarget.length = 0;},' +
+						'info: function() {dolog("info", arguments);},' +
+						'log: function() {dolog("log", arguments);},' +
+						'warn: function() {dolog("warn", arguments);},' +
+						'error: function() {dolog("error", arguments);},' +
+					'};' +
+				'})(extras)) : undefined);' +
+				pre +
+				'extras = undefined;' +
+				'return (function({' + parameters.join(',') + '}) {\n' +
+					code + '\n' + after +
+				'}).call(parameters["this"] || {}, parameters);' +
+			'};\n'
+		);
 
-			let fn = null;
-			let compileError = null;
+		let fn = null;
+		let compileError = null;
 
-			const begin = performance.now();
-			try {
-				importScripts(URL.createObjectURL(new Blob(
-					[src],
-					{type: 'text/javascript'}
-				)));
-				fn = self.tempFn.bind({});
-				self.tempFn = null;
-			} catch(e) {
-				// WORKAROUND (Safari): blobs inaccessible when run
-				// from the filesystem, so fall-back to a nasty eval
-				if(e.toString().includes('DOM Exception 19')) {
-					try {
-						/* jshint evil: true */
-						eval(src);
-						fn = self.tempFn.bind({});
-						self.tempFn = null;
-					} catch(e2) {
-						compileError = stringifyCompileError(e2);
-					}
-				} else {
-					compileError = stringifyCompileError(e);
+		const begin = performance.now();
+		try {
+			importScripts(URL.createObjectURL(new Blob(
+				[src],
+				{type: 'text/javascript'}
+			)));
+			fn = self.tempFn.bind({});
+			self.tempFn = null;
+		} catch(e) {
+			// WORKAROUND (Safari): blobs inaccessible when run
+			// from the filesystem, so fall-back to a nasty eval
+			if(e.toString().includes('DOM Exception 19')) {
+				try {
+					/* jshint evil: true */
+					eval(src);
+					fn = self.tempFn.bind({});
+					self.tempFn = null;
+				} catch(e2) {
+					compileError = stringifyCompileError(e2);
 				}
+			} else {
+				compileError = stringifyCompileError(e);
 			}
-			const compileTime = performance.now() - begin;
+		}
+		const compileTime = performance.now() - begin;
 
-			return {fn, compileError, compileTime};
-		},
+		return {fn, compileError, compileTime};
+	}
 
-		stringifyEntryError,
+	function load(site, qid, progressCallback) {
+		const loaderWorker = workerUtils.make(pathLoaderWorker);
 
-		load: (site, qid, progressCallback) => {
-			const loaderWorker = workerUtils.make(pathLoaderWorker);
-
-			return new Promise((resolve, reject) => {
-				loaderWorker.addEventListener('message', (event) => {
-					const data = event.data;
-					if(data.error) {
-						loaderWorker.terminate();
-						reject(data.error);
-						return;
-					}
-					if(!data.entries) {
-						if(progressCallback) {
-							progressCallback(data.loaded, data.total);
-						}
-						return;
-					}
+		return new Promise((resolve, reject) => {
+			loaderWorker.addEventListener('message', (event) => {
+				const data = event.data;
+				if(data.error) {
 					loaderWorker.terminate();
-					resolve(data.entries.map(parseEntry));
-				});
-
-				loaderWorker.postMessage({site, qid});
+					reject(data.error);
+					return;
+				}
+				if(!data.entries) {
+					if(progressCallback) {
+						progressCallback(data.loaded, data.total);
+					}
+					return;
+				}
+				loaderWorker.terminate();
+				resolve(data.entries.map(parseEntry));
 			});
-		},
+
+			loaderWorker.postMessage({site, qid});
+		});
+	}
+
+	return {
+		compile,
+		stringifyEntryError,
+		load,
 	};
 });
